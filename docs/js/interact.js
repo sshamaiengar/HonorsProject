61b9764461e3c895ba112f47826eb1515d9f8b91
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2017-04-26 17:55:31 -0400

    [ADD] GameDev intro, Breakout assets, and beta GameDev editor Issue: Unable to load Phaser into Iframe because of different domains (“null” vs “file://“), will try on live domain

--- docs/js/interact.js
+++ docs/js/interact.js
@@ -74,6 +74,7 @@ function activateWebEditor(number){
 	document.getElementById("frame"+number).appendChild(iframe);
 
 	$("#run"+number).click(function(){
+		
 		$("#iframe"+number).remove();
 		var iframe = document.createElement("iframe");
 		iframe.id = "iframe"+number;
@@ -102,5 +103,66 @@ function activateWebEditor(number){
 		iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(editor.getValue());
 		document.getElementById("frame"+number).appendChild(iframe);
 		$("#indicator"+number).removeClass("fa-check").removeClass("fa-times").css('display', 'none');
-	})
+	});
+}
+
+function activateGameEditor(number){
+	var editor = ace.edit("editor"+number);
+	editor.setTheme("ace/theme/snappy-light");
+	editor.setHighlightActiveLine(false);
+	editor.setShowPrintMargin(false);
+	editor.container.style.lineHeight = 1.7
+	editor.renderer.updateFontSize()
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.getSession().setUseSoftTabs(false);
+	editor.getSession().on('change', function(e) {
+		$("#editorContent"+number).html(editor.getValue());
+	});
+
+	var templateStart = "<html><head></head><body><scr"+"ipt type='text/javascript'>";
+	var templateEnd = "</scr"+"ipt></body></html>";
+
+	// reset iframe using default code
+	$("#iframe"+number).remove();
+	var iframe = document.createElement("iframe");
+	iframe.id = "iframe"+number;
+	// $("iframe"+number).load(function(){
+	// 	$(this).contents().find('body').append(templateStart+editor.getValue() + templateEnd);
+	// 	console.log('load');
+	// });
+	iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(templateStart + editor.getValue() + templateEnd);
+	iframe.setAttribute("name", "iframe"+number);
+	document.getElementById("frame"+number).appendChild(iframe);
+
+	$("#run"+number).click(function(){
+		
+		$("#iframe"+number).remove();
+		var iframe = document.createElement("iframe");
+		iframe.id = "iframe"+number;
+		iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(templateStart + editor.getValue() + templateEnd);
+		iframe.setAttribute("name", "iframe"+number);
+		document.getElementById("frame"+number).appendChild(iframe);
+
+		var answerRegex = new RegExp($("#answer"+number).text());
+		// var correct = editor.getValue().indexOf($("#answer"+number).text()) != -1 ? true : false;
+		// console.log(editor.getValue());
+		// console.log(editor.getValue().match(answerRegex));
+		var correct = editor.getValue().match(answerRegex) != null ? true: false;
+		if (correct){
+			$("#indicator"+number).removeClass("fa-times").addClass("fa-check");
+			$("#indicator"+number).css('display', 'inline');
+		} else {
+			$("#indicator"+number).removeClass("fa-check").addClass("fa-times");
+			$("#indicator"+number).css('display', 'inline');
+		}
+	});
+	$("#reset"+number).click(function(){
+		editor.setValue($("#defaultCode"+number).text());
+		$("#iframe"+number).remove();
+		var iframe = document.createElement("iframe");
+		iframe.id = "iframe"+number;
+		iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(templateStart + editor.getValue() + templateEnd);
+		document.getElementById("frame"+number).appendChild(iframe);
+		$("#indicator"+number).removeClass("fa-check").removeClass("fa-times").css('display', 'none');
+	});
 }
